7aee5238780224a136503c09ce6718bb2772d604
AMadverick1 <madonsela.ano22@gmail.com>
2024-10-24 03:14:04 +0200

    port 5000 update

--- frontend/acc-system/src/services/api.js
+++ frontend/acc-system/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Set up the axios instance
 const api = axios.create({
-  baseURL: 'https://https://master.d1ett1tvi16xqx.amplifyapp.com:5000',
+  baseURL: 'https://master.d1ett1tvi16xqx.amplifyapp.com:5000',
 });
 
 export default api;
