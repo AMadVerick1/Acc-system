0cdb19b0f92304acceaa676c1b6aa3b455c19774
AMadverick1 <madonsela.ano22@gmail.com>
2024-10-27 21:17:50 +0200

    http version

--- backend/index.js
+++ backend/index.js
@@ -63,7 +63,7 @@ connectDB().then(() => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT  = process.env.PORT || 5000;
 app.listen(PORT, () => 
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
--- frontend/acc-system/src/services/api.js
+++ frontend/acc-system/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Set up the axios instance
 const api = axios.create({
-  baseURL: 'https://67.202.33.127:5000',
+  baseURL: 'http://67.202.33.127:5000',
 });
 
 export default api;
