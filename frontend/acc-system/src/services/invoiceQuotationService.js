af00ffde3feb51ca61c4f0a0d3657e6dd36883f9
AMadverick1 <madonsela.ano22@gmail.com>
2024-10-29 07:29:53 +0200

    running it back to https

--- frontend/acc-system/src/services/invoiceQuotationService.js
+++ frontend/acc-system/src/services/invoiceQuotationService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = 'https://67.202.33.127:5000';
 
 export const getAllInvoiceQuotations = async () => {
     try {
